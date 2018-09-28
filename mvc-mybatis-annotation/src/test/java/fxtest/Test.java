a031613cc9660d8f27beb20d1f0f3dd41425da5b
fuxiyang <fuxi_yang@163.com>
2018-09-29 00:48:50 +0800

    提交测试

--- mvc-mybatis-annotation/src/test/java/fxtest/Test.java
+++ mvc-mybatis-annotation/src/test/java/fxtest/Test.java
@@ -74,12 +74,12 @@ public class Test {
 //        entity.setCjsj(System.currentTimeMillis());
 //        entity.setGxsj(System.currentTimeMillis());
 //        service.insert(entity);
-i
+
 //        //selectById
 //        IncidentEntity entity=service.selectById("as26899312de564g85sde");
 //        System.out.println(entity);
 //        //update
-//        entity.setCzz("测试");
+//        entity.setCzz("测试");us
 //        service.update(entity);
 
         //delete
